6237491505a10caaae5910727659976abd3515f9
ViniMacacari <macacarivinicius@gmail.com>
2025-12-19 10:12:18 -0300

    feat: nova classe para manipulação de xml

--- bin-audiodata/event-system/bin-0tve00cp-event.ts
+++ bin-audiodata/event-system/bin-0tve00cp-event.ts
@@ -3,6 +3,268 @@ import { FifaUtil } from '../../utils/fifa-util'
 
 export namespace AudioBin {
     export namespace EventSystem {
+        export namespace AudioBin {
+            export namespace EventSystem {
+                export interface BinaryReaderLike {
+                    baseStream: { position: number }
+                    readUInt16(): number
+                    readInt32(): number
+                }
+
+                export interface EventLike {
+                    Name: string
+                    InterfaceCrc: number
+                    NumParameters: number
+                    ParameterIds: number[]
+                }
+
+                export interface ParameterValueLike {
+                    Name: string
+                    Value: number
+                }
+
+                export interface ParameterLike {
+                    Type: number
+                    Id: number
+                    NumValues: number
+                    Name: string
+                    ParameterValues: ParameterValueLike[]
+                }
+
+                export class EventSystemFile {
+                    SystemCrc = 0
+                    NumEvents = 0
+                    NumParameters = 0
+
+                    OffsetOffsetsEvents = 0
+                    OffsetOffsetsParameters = 0
+
+                    OffsetsEvents: number[] = []
+                    OffsetsParameters: number[] = []
+
+                    Events: EventLike[] = []
+                    Parameters: ParameterLike[] = []
+
+                    constructor()
+                    constructor(r: BinaryReaderLike)
+                    constructor(r?: BinaryReaderLike) {
+                        if (r) this.load(r)
+                    }
+
+                    private load(r: BinaryReaderLike): void {
+                        r.baseStream.position = 8
+
+                        r.readUInt16()
+                        r.readUInt16()
+                        r.readUInt16()
+                        r.readUInt16()
+
+                        this.SystemCrc = r.readInt32()
+                        this.NumEvents = r.readInt32()
+                        this.NumParameters = r.readInt32()
+
+                        this.OffsetOffsetsEvents = r.readInt32()
+                        this.OffsetOffsetsParameters = r.readInt32()
+
+                        r.baseStream.position = this.OffsetOffsetsEvents
+                        this.OffsetsEvents = new Array(this.NumEvents)
+                        for (let i = 0; i < this.OffsetsEvents.length; i++) {
+                            this.OffsetsEvents[i] = r.readInt32()
+                        }
+
+                        r.baseStream.position = this.OffsetOffsetsParameters
+                        this.OffsetsParameters = new Array(this.NumParameters)
+                        for (let i = 0; i < this.OffsetsParameters.length; i++) {
+                            this.OffsetsParameters[i] = r.readInt32()
+                        }
+
+                        this.Events = new Array(this.NumEvents)
+                        for (let i = 0; i < this.Events.length; i++) {
+                            r.baseStream.position = this.OffsetsEvents[i]
+                            this.Events[i] = new (Event as any)(r)
+                        }
+
+                        this.Parameters = new Array(this.NumParameters)
+                        for (let i = 0; i < this.Parameters.length; i++) {
+                            r.baseStream.position = this.OffsetsParameters[i]
+                            this.Parameters[i] = new (Parameter as any)(r)
+                        }
+                    }
+
+                    ToXml(fileName: string): string {
+                        let strXml = ''
+
+                        this.Parameters = [...this.Parameters].sort((a, b) => a.Id - b.Id)
+                        this.Events = [...this.Events].sort((a, b) => (a.Name < b.Name ? -1 : a.Name > b.Name ? 1 : 0))
+
+                        strXml += '<AudioFramework>'
+                        strXml +=
+                            '\n  <Module type="' +
+                            this.GetModuleType(fileName) +
+                            '" name="' +
+                            this.GetModuleName(fileName) +
+                            '"' +
+                            this.GetModuleExtra(fileName) +
+                            '>'
+                        strXml +=
+                            '\n    <EventSystem systemCrc="' +
+                            String(this.SystemCrc) +
+                            '" numParameters="' +
+                            String(this.NumParameters) +
+                            '" numEvents="' +
+                            String(this.NumEvents) +
+                            '">'
+                        strXml += '\n      <Version major="1" minor="2" patch="4" />'
+
+                        for (let i = 0; i < this.NumParameters; i++) {
+                            const p = this.Parameters[i]
+
+                            switch (p.Type) {
+                                case (Parameter as any).BinParameterType?.EnumBit ?? 1: {
+                                    strXml +=
+                                        '\n      <enum name="' +
+                                        p.Name +
+                                        '" id="' +
+                                        String(p.Id) +
+                                        '" type="EnumBit" numValues="' +
+                                        String(p.NumValues) +
+                                        '">'
+                                    for (let j = 0; j < p.NumValues; j++) {
+                                        const pv = p.ParameterValues[j]
+                                        strXml += '\n        <enumerator name="' + pv.Name + '" value="' + String(pv.Value) + '" />'
+                                    }
+                                    strXml += '\n      </enum>'
+                                    break
+                                }
+
+                                case (Parameter as any).BinParameterType?.Int ?? 3: {
+                                    strXml += '\n      <parameter name="' + p.Name + '" id="' + String(p.Id) + '" type="Int" />'
+                                    break
+                                }
+                            }
+                        }
+
+                        for (let i = 0; i < this.NumEvents; i++) {
+                            const e = this.Events[i]
+
+                            if (e.NumParameters === 0) {
+                                strXml +=
+                                    '\n      <function name="' +
+                                    e.Name +
+                                    '" interfaceCrc="' +
+                                    String(e.InterfaceCrc) +
+                                    '" numParameters="' +
+                                    String(e.NumParameters) +
+                                    '" />'
+                            } else {
+                                strXml +=
+                                    '\n      <function name="' +
+                                    e.Name +
+                                    '" interfaceCrc="' +
+                                    String(e.InterfaceCrc) +
+                                    '" numParameters="' +
+                                    String(e.NumParameters) +
+                                    '">'
+                                for (let j = 0; j < e.NumParameters; j++) {
+                                    const parameterIndex = this.GetParameterIndex(e.ParameterIds[j])
+                                    const p = this.Parameters[parameterIndex]
+                                    strXml +=
+                                        '\n        <parameter type="' +
+                                        p.Name +
+                                        '" name="' +
+                                        p.Name +
+                                        '" parameterId="' +
+                                        p.Id +
+                                        '" />'
+                                }
+                                strXml += '\n      </function>'
+                            }
+                        }
+
+                        strXml += '\n    </EventSystem>'
+                        strXml += '\n  </Module>'
+                        strXml += '\n</AudioFramework>'
+
+                        return strXml
+                    }
+
+                    private GetParameterIndex(parameterId: number): number {
+                        for (let i = 0; i < this.NumParameters; i++) {
+                            if (this.Parameters[i].Id === parameterId) return i
+                        }
+                        return 0
+                    }
+
+                    private GetModuleType(fileName: string): string {
+                        fileName = this.getFileNameWithoutExtension(fileName)
+
+                        switch (true) {
+                            case fileName.includes('announcer_eventsystem'):
+                                return 'SpeechModule'
+                            case fileName.includes('commentary_eventsystem'):
+                                return 'SpeechModule'
+                            case fileName.includes('playercalls_eventsystem'):
+                                return 'SpeechModule'
+                            case fileName === 'chant_eventsystem':
+                                return 'GraffitiPlayerModule'
+                            case fileName === 'crowd_eventsystem':
+                                return 'ContextModule'
+                            case fileName === 'commentary_triggers_eventsystem':
+                                return 'ContextModule'
+                        }
+
+                        return ''
+                    }
+
+                    private GetModuleName(fileName: string): string {
+                        fileName = this.getFileNameWithoutExtension(fileName)
+
+                        switch (true) {
+                            case fileName.includes('announcer_eventsystem'):
+                                return 'Announcer'
+                            case fileName.includes('commentary_eventsystem'):
+                                return 'CommentarySpeech'
+                            case fileName.includes('playercalls_eventsystem'):
+                                return 'PlayerCalls'
+                            case fileName === 'chant_eventsystem':
+                                return 'GraffitiPlayer'
+                            case fileName === 'crowd_eventsystem':
+                                return 'CrowdContextModule'
+                            case fileName === 'commentary_triggers_eventsystem':
+                                return 'ContextModule'
+                        }
+
+                        return ''
+                    }
+
+                    private GetModuleExtra(fileName: string): string {
+                        fileName = this.getFileNameWithoutExtension(fileName)
+
+                        switch (true) {
+                            case fileName === 'crowd_eventsystem':
+                                return ' maxNumContexts="750" maxNumGroups="100" maxNumValidContexts="400" maxNumTriggeredContexts="100" numPassives="150" priorityDecayRate="1000"'
+                            case fileName === 'commentary_triggers_eventsystem':
+                                return ' maxNumContexts="1200" maxNumGroups="100" maxNumValidContexts="500" maxNumTriggeredContexts="150" numPassives="150" priorityDecayRate="1000"'
+                        }
+
+                        return ''
+                    }
+
+                    private getFileNameWithoutExtension(p: string): string {
+                        const normalized = p.replace(/\\/g, '/')
+                        const base = normalized.split('/').pop() ?? normalized
+                        const lastDot = base.lastIndexOf('.')
+                        return lastDot >= 0 ? base.slice(0, lastDot) : base
+                    }
+                }
+
+                declare const Event: new (r: BinaryReaderLike) => EventLike
+                declare const Parameter: new (r: BinaryReaderLike) => ParameterLike & {
+                    BinParameterType?: { EnumBit: number; Int: number }
+                }
+            }
+        }
+
         export class ParameterValue {
             Unknown_1 = 0
             Value = 0
